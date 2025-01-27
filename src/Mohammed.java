2ecb1e5a82400c095b4bd601f0d9e074516184a7
Nick43521 <mdshaik.info@email.com>
2025-01-27 16:22:40 +0530

    updated code

---
+++ src/Mohammed.java
@@ -0,0 +1,12 @@
+
+public class Mohammed {
+static String name = "mohammed ";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String n1 = name.concat("shaik");
+
+
+System.out.println(n1);
+	}
+
+}
